88500e9108a435748f67165e6a5f756994c3e602
Poorva Gupta <mail2poorva@gmail.com>
2021-10-14 11:01:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/photos?_start=0&_limit=50")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>{" "}
+        </div>
+      );
+
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1>{" "}
+        {items.map((item) => (
+          <div key={item.id} className="main_div">
+            <h3>{item.title}</h3>
+            <img src={item.url} alt="im" id="image"></img>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
